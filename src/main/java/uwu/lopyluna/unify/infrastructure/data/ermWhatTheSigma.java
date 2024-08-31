ad769424f1230840d1b618f3d36902947f9b38c7
xuwu / luna <loganalexzand@gmail.com>
2024-08-30 20:51:12 -0800

    Mod Metal Conversion Hehehe Ha

--- src/main/java/uwu/lopyluna/unify/infrastructure/data/ermWhatTheSigma.java
+++ src/main/java/uwu/lopyluna/unify/infrastructure/data/ermWhatTheSigma.java
@@ -1,60 +1,84 @@
 package uwu.lopyluna.unify.infrastructure.data;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.ItemLike;
-import net.minecraftforge.registries.ForgeRegistries;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @SuppressWarnings("unused")
 public class ermWhatTheSigma {
 
-    private static List<String> unify$getAllIDs() {
+    public static List<String> getAllIDs() {
         List<String> entry = new ArrayList<>();
-        entry.addAll(unify$getIDs("PLATINUM", true));
-        entry.addAll(unify$getIDs("LEAD", true));
-        entry.addAll(unify$getIDs("SILVER", true));
-        entry.addAll(unify$getIDs("TIN", true));
-        entry.addAll(unify$getIDs("URANIUM", true));
-        entry.addAll(unify$getIDs("ALUMINUM", true));
-        entry.addAll(unify$getIDs("NICKEL", true));
-        entry.addAll(unify$getIDs("TUNGSTEN", true));
+        entry.addAll(getIDs("PLATINUM", true));
+        entry.addAll(getIDs("LEAD", true));
+        entry.addAll(getIDs("SILVER", true));
+        entry.addAll(getIDs("TIN", true));
+        entry.addAll(getIDs("URANIUM", true));
+        entry.addAll(getIDs("ALUMINUM", true));
+        entry.addAll(getIDs("NICKEL", true));
+        entry.addAll(getIDs("TUNGSTEN", true));
 
-        entry.addAll(unify$getIDs("STEEL", false));
-        entry.addAll(unify$getIDs("ELECTRUM", false));
-        entry.addAll(unify$getIDs("BRONZE", false));
-        entry.addAll(unify$getIDs("CAST_IRON", false));
-        entry.addAll(unify$getIDs("INVAR", false));
-        entry.addAll(unify$getIDs("CONSTANTAN", false));
-        entry.addAll(unify$getIDs("WROUGHT_IRON", false));
-        entry.addAll(unify$getIDs("TARNISHED_GOLD", false));
+        entry.addAll(getIDs("STEEL", false));
+        entry.addAll(getIDs("ELECTRUM", false));
+        entry.addAll(getIDs("BRONZE", false));
+        entry.addAll(getIDs("CAST_IRON", false));
+        entry.addAll(getIDs("INVAR", false));
+        entry.addAll(getIDs("CONSTANTAN", false));
+        entry.addAll(getIDs("WROUGHT_IRON", false));
+        entry.addAll(getIDs("TARNISHED_GOLD", false));
         return entry;
     }
 
-    private static List<String> unify$getIDs(String name, boolean isOre) {
+    public static List<String> getIDs(String name, boolean isOre) {
         String id = name.toLowerCase();
         List<String> asIDs = new ArrayList<>();
+        if (name.equals("CAST_IRON")) {
+            asIDs.add("industrial_iron_ingot");
+            asIDs.add("industrial_iron_nugget");
+            asIDs.add("industrial_iron_sheet");
+            asIDs.add("industrial_iron_block");
+        }
+        if (name.equals("ALUMINUM")) {
+            if (isOre) {
+                String rawId = "bauxite";
+                asIDs.add("raw_" + rawId);
+                asIDs.add("raw_" + rawId + "_block");
+                asIDs.add(rawId + "_ore");
+                asIDs.add("deepslate_" + rawId + "_ore");
+            }
+
+        }
+        if (name.equals("TUNGSTEN")) {
+            if (isOre) {
+                asIDs.add("raw_tungsten_ore");
+                String rawId = "wolfram";
+                asIDs.add("raw_" + rawId);
+                asIDs.add("raw_" + rawId + "_block");
+                asIDs.add(rawId + "_ore");
+                asIDs.add("deepslate_" + rawId + "_ore");
+            }
+        }
+
         asIDs.add(id + "_ingot");
         asIDs.add(id + "_nugget");
         asIDs.add(id + "_sheet");
+        asIDs.add(id + "_plate");
         asIDs.add(id + "_block");
 
+        asIDs.add("ingot_" + id);
+        asIDs.add("nugget_" + id);
+        asIDs.add("sheet_" + id);
+        asIDs.add("plate_" + id);
+        asIDs.add("block_" + id);
+
         if (isOre) {
+            asIDs.add(id + "_raw");
             asIDs.add("raw_" + id);
             asIDs.add("raw_" + id + "_block");
+            asIDs.add(id + "_raw_block");
             asIDs.add(id + "_ore");
+            asIDs.add("ore_" + id);
             asIDs.add("deepslate_" + id + "_ore");
         }
         return asIDs;
-    }
-
-    private static String unify$safeId(ItemLike registryEntry) {
-        return unify$safeName(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(registryEntry.asItem()))).replace("unify:", "");
-    }
-
-    private static String unify$safeName(ResourceLocation id) {
-        return id.getPath().replace('/', '_');
     }
 }
